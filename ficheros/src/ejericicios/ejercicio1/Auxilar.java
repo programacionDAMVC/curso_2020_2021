8c377b71110397aabd9fa7343a81e6f511aa34ac
manuel molino <programacionpsdam@gmail.com>
2021-04-12 13:11:20 +0200

    subida programa parcial

--- ficheros/src/ejericicios/ejercicio1/Auxilar.java
+++ ficheros/src/ejericicios/ejercicio1/Auxilar.java
@@ -1,8 +1,11 @@
 package ejericicios.ejercicio1;
 
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -12,7 +15,7 @@ public class Auxilar {
     public static List<Persona> extraerListaDesdeFichero (String path) throws IOException {
         List<Persona> listaPersona = new ArrayList<>();
         Path pathFile = Paths.get(path);
-        Scanner in = new Scanner(pathFile);
+        /*Scanner in = new Scanner(pathFile);
         while (in.hasNextLine()) {
             String linea = in.nextLine();
             String[] tokens = linea.split(",");
@@ -27,25 +30,80 @@ public class Auxilar {
             Persona persona = new Persona(firstName, lastName, email, gender, country);
             listaPersona.add(persona);
         }
-        in.close();
+        if (in != null)
+            in.close();*/
+        List<String> lineas = Files.readAllLines(pathFile);
+        for (String linea: lineas) {
+            String[] tokens = linea.split(",");
+            Gender gender    = Gender.male;
+            if (tokens[3].equalsIgnoreCase("female"))
+                gender = Gender.female;
+            listaPersona.add(new Persona(tokens[0], tokens[1], tokens[2], gender, tokens[4]));
+        }
         return listaPersona;
         //repetir la lógica usando readAllLines de la clase Files
     }
-    public static void crearFicheroPersonal (List<Persona> listaPersonas) {
+    public static void crearFicheroPersonal (List<Persona> listaPersonas) throws IOException {
         //salida del fichero sea personal_9_2_2021_12_31.csv
         LocalDateTime now = LocalDateTime.now();
         StringBuilder stringBuilder = new StringBuilder();
-        String nombreFichero = "";
+        stringBuilder.append("EXERCISES_FILES/personal_");
+        stringBuilder.append(now.getDayOfMonth());
+        stringBuilder.append("_");
+        stringBuilder.append(now.getMonthValue());
+        stringBuilder.append("_");
+        stringBuilder.append(now.getYear());
+        stringBuilder.append("_");
+        stringBuilder.append(now.getHour());
+        stringBuilder.append("_");
+        stringBuilder.append(now.getMinute());
+        stringBuilder.append(".csv");
+        String nombreFichero = stringBuilder.toString();
+        File outFile = new File(nombreFichero);
         //PrintWriter primera opción
-        //BufferedWriter de la clase Files
+        PrintWriter out = new PrintWriter(outFile);
+        for (Persona persona: listaPersonas) {
+            out.println(persona);
+            out.flush();
+        }
+        if (out != null)
+            out.close();
+        if (outFile.length() > 0)
+            System.out.printf("Creado fichero %s de %d bytes%n", outFile.getName(), outFile.length() );
+        else
+            System.out.println("no se ha podido crear el fichero");
+
+        /*//BufferedWriter de la clase Files
+        Path path = Paths.get(nombreFichero);
+        BufferedWriter out = Files.newBufferedWriter(path, StandardCharsets.UTF_8);
+        for (Persona persona: listaPersonas) {
+            out.write(persona.toString());
+            out.newLine();
+            out.flush();
+        }
+        if (out != null)
+            out.close();
+        if (path.toFile().length() > 0)
+            System.out.printf("Creado fichero %s de %d bytes%n", path.getFileName(), path.toFile().length() );
+        else
+            System.out.println("no se ha podido crear el fichero");*/
     }
 
     public static void main(String[] args) {
         try {
-            extraerListaDesdeFichero("EXERCISES_FILES/persnal.csv").forEach(System.out::println);
+            extraerListaDesdeFichero("EXERCISES_FILES/personal.csv").forEach(System.out::println);
         } catch (IOException e) {
             e.printStackTrace();
         }
-
+       /* List<Persona> lista = new ArrayList<>();
+        lista.add(new Persona("nombre1", "apellidos1", "email1", Gender.female, "pais1" ));
+        lista.add(new Persona("nombre2", "apellidos2", "email2", Gender.male, "pais2" ));
+        try {
+            crearFicheroPersonal(lista);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
     }
 }
---
+++ ficheros/src/ejericicios/ejercicio1/TestPersonal.java
@@ -0,0 +1,10 @@
+package ejericicios.ejercicio1;
+
+public class TestPersonal {
+    public static void main(String[] args) {
+        //crear List<Persona> usando el método correspondiente de la clase auxiliar, usando el fichero personal.csv
+        //crear una instancia de Personal, necesito List<Persona> creada anteriormente
+        //compruebo mostrando en consola la salida del método obtenerListaPersonasPorPais (String pais) de la clase
+        //personal, usando la referencia del objeto Personal y como pais ponemos Poland
+    }
+}
