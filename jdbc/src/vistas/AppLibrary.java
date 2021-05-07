e9e78dee353867208aa31dda652ae0d2c7e038c7
manuel molino <programacionpsdam@gmail.com>
2021-05-07 14:43:42 +0200

    modificado:     jdbc/.idea/uiDesigner.xml 	nuevo archivo:  jdbc/src/vistas/AppLibrary.form 	nuevo archivo:  jdbc/src/vistas/AppLibrary.java 	modificado:     jdbc/src/vistas/VistaIntroduccion.form 	modificado:     jdbc/src/vistas/VistaIntroduccion.java

---
+++ jdbc/src/vistas/AppLibrary.java
@@ -0,0 +1,16 @@
+package vistas;
+
+import javax.swing.*;
+
+public class AppLibrary {
+    private JPanel mainPanel;
+    private JPanel topPanel;
+    private JLabel titleAppLabel;
+    private JPanel bottonmPanel;
+    private JButton buttonCreate;
+    private JButton buttonPlus;
+    private JButton buttonUpdaate;
+    private JButton buttonMinus;
+    private JButton deleteButton;
+    private JPanel centralPanel;
+}
--- jdbc/src/vistas/VistaIntroduccion.java
+++ jdbc/src/vistas/VistaIntroduccion.java
@@ -1,6 +1,9 @@
 package vistas;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class VistaIntroduccion {
     private JPanel panelPrincipal;
@@ -12,13 +15,33 @@ public class VistaIntroduccion {
     private JPanel panelCentral;
     private JLabel etiquetaNombre;
     private JTextField textFieldNombre;
+    private JButton botonCierre;
+
+    public VistaIntroduccion() {
+        //registro de eventos
+        botonMayuscula.addActionListener( e -> {
+            //System.out.println("Me acabas de pulsar");
+            String nombre = textFieldNombre.getText(); //
+            String nombreMayuscula = nombre.toUpperCase();
+            System.out.println(nombreMayuscula);
+            textFieldNombre.setText(nombreMayuscula);
+        });
+        botonCierre.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                System.exit(0);
+            }
+        });
+    }
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("título de la ventana");
         VistaIntroduccion vista = new VistaIntroduccion();
         frame.setContentPane(vista.panelPrincipal);
-        frame.setSize(400, 400);
+        Dimension pantalla = Toolkit.getDefaultToolkit().getScreenSize();
+        frame.setSize(pantalla.width / 3, pantalla.height / 2);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null); //CENTRAMOS LA VENTANA
         frame.setVisible(true);
     }
 }
